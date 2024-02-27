960b12b8c34b7cf79200fa8ca7e6c07feae1b990
stg <a.sergiotorres@gmail.com>
2024-02-27 15:26:40 +0100

    fix: new DB connection works now. dotenv/config imported.

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata"
+import "dotenv/config"
 import { DataSource } from "typeorm"
 import { Roles1708989507982 } from "./migrations/1708989507982-roles"
 import { Users1708989530865 } from "./migrations/1708989530865-users"
 import { Services1708989550470 } from "./migrations/1708989550470-services"
 import { Appointments1708989564625 } from "./migrations/1708989564625-appointments"
+import { configDotenv } from "dotenv"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
